2ffed70fe8519b01f3702a01381c8ddbb7f846c6
ut21040164 <utm21040164@utma.edu.mx>
2023-06-27 10:32:21 -0600

    Design

---
+++ JS/carousel.js
@@ -0,0 +1,13 @@
+var links = ["../IMG/carreras/Mecatronica.jpg","../IMG/carreras/Nanotecnologia.jpg","../IMG/carreras/Negocios.jpg", "../IMG/carreras/Tics.jpeg"];
+var current = 0;
+
+function cambio() {
+    let car = document.getElementById('carousel');
+    car.style.backgroundImage = "url('/IMG/" + links[current] + "')";
+    current++;
+    if (current >= links.length) {
+        current = 0;
+    }
+}
+
+setInterval(cambio, 2000);
